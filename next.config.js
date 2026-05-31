5f246434c3c58011db89edd61a6104ac1cfba50d
Felipe França <felipe.franca.108@ufrn.edu.br>
2026-05-31 03:29:36 -0300

    perf: enable optimizePackageImports for smaller bundles
    Tree-shake @portabletext/react and @sanity/client to reduce
    unused JavaScript in page bundles.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ["@portabletext/react", "@sanity/client"],
+  },
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
